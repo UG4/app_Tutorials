641c1ca493051046b2a67f8898821f03a0fa6782
Sebastian Reiter <sebastian.reiter@gcsc.uni-frankfurt.de>
2014-12-09 06:25:10 +0000

    Fixed tut07_parallel_laplace_gmg, now using load_balancing_util.lua.
    Removed some old tutorials which just didn't make any sense any more

--- tut07_parallel_laplace_gmg.lua
+++ tut07_parallel_laplace_gmg.lua
@@ -11,6 +11,7 @@
 --------------------------------------------------------------------------------
 -- include the basic util-methods.
 ug_load_script("ug_util.lua")
+ug_load_script("util/load_balancing_util.lua")
 
 -- Get the command line parameters
 dim = util.GetParamNumber("-dim", 2)
@@ -22,171 +23,67 @@ InitUG(dim, AlgebraType("CPU", 1))
 
 gridName = util.GetParam("-grid", "unit_square/unit_square_quads_2x2.ugx")
 outFileNamePrefix = util.GetParam("-o", "distributed_domain_")
-numPreRefs = util.GetParamNumber("-numPreRefs", 1)
-numMidRefs = util.GetParamNumber("-numMidRefs", 1)
-numPostRefs = util.GetParamNumber("-numPostRefs", 1)
+numRefs = util.GetParamNumber("-numRefs", 4)
 debug = util.HasParamOption("-debug")
 
 -- We will save the created hierarchy to this file (with appended process id)
 outHierarchyFilePrefix = util.GetParam("-oh", "hierarchy_on_proc_")
 
+
+
+-- while this is not necessary, we can overwrite some of the default parameters
+-- used for load-balancing.
+-- The redistProcs parameter defines the maximum number of processes to which
+-- the grid is distributed from a single process. If one runs the application on
+-- 'redistProcs' this will lead to a process hierarchy, which is a nice thing
+-- for massively parallel runs.
+balancer.redistProcs = 64
+
+-- we have to give the balancer a chance to initialize its parameters from
+-- command line arguments:
+balancer.ParseParameters()
+
+
+
 -- Create the domain and load a grid
 dom = Domain()
-
+print("loading grid from " .. gridName .. "...")
 LoadDomain(dom, gridName)
 
--- Now that we're here, the domain was successfully loaded
-print("Loaded domain from " .. gridName)
+-- This will automatically refine and rebalance the domain. If you need more
+-- control, please have a look at the implementation of this method in
+-- scripts/util/load_balancing_util.lua and extract the pieces you need.
+-- You can e.g. create your own LoadBalancer with custom weightings etc and use
+-- that during redistribution or use a different refinement scheme.
+balancer.RefineAndRebalanceDomain(dom, numRefs)
 
-
--- We'll create a global refiner and prerefine the domain before distribution starts.
-refiner = GlobalDomainRefiner(dom)
-
--- perform pre-refinement
-for i = 1, numPreRefs do
-	print("refining...")
-	refiner:refine()
+-- since we used standard methods, we can now query the default load balancer
+-- for distribution qualities
+if balancer.defaultBalancer ~= nil then
+	balancer.defaultBalancer:print_quality_records()
 end
 
---	This demonstration requires 4 processes
-if NumProcs() < 4 then
-	print("At least 4 processes are required for this script. Run this script with e.g.")
-	print("mpirun -np 4 ugshell -ex tutorials/tut07_parallel_laplace_gmg.lua")
-	print("Aborting.")
-	exit()
-end
-
---	First we'll create a partition map. This is done on all processes, even
---	though the grid is currently only on process 0.
-partitionMap = PartitionMap()
-
---	We'll also store the rank of the current process
-procRank = ProcRank()
-
---	first we'll distribute sections of the domain to processes 0, 4, 8 and 12
---firstTargets = {0, 4, 8, 12}
-firstTargets = {0, 2}
-
---	Since only process 0 has a grid at the moment, we'll only fill the
---	partition map on him.
-if procRank == 0 then
-	-- Iterate over all entries in firstTargets
-	for i, target in ipairs(firstTargets) do
-		partitionMap:add_target_proc(target)
-	end
-	
-	-- The following call performs the load-balancing and partitions the
-	-- domain into several sections, which will then be sent to the different
-	-- processes.
-	-- Note that several different load-balancers exist. In this example
-	-- we'll simply use a regular-grid with 2x2 cells.
-	--
-	-- Note that the i-th target corresponds to the cell with indices cx, cy,
-	-- where i = cy * numCellsY + cx	(numCellsX and numCellsY are specified
-	-- during the call to PartitionDomain_RegularGrid).
-	--
-	-- For this algorithm it is crucial that the number of processes in the
-	-- partitionMap match the number of cells specified for the load-balancer.
-	-- The last parameter says that only surface-elements shall be partitioned
-	PartitionDomain_RegularGrid(dom, partitionMap, 2, 1, true)
-	
-	-- We'll save the partition map. This should only be done for debugging.
-	SaveGrid(dom:grid(), partitionMap:get_partition_handler(),
-		"partitionMap_1_p" .. procRank .. ".ugx")
-end
-
-
---	The partition map is set up. We can now distribute the domain.
---	DistributeDomain takes three parameters: The domain, the
---	partitionMap, and a boolean indicating whether vertical interfaces
---	shall be created.
-print("\nPERFORMING INITIAL DISTRIBUTION")
-if DistributeDomain(dom, partitionMap, true) == false then
-	print("First redistribution failed. Please check your partitionMap.")
-	exit()
-end
-
-if debug == true then
-	if TestDomainInterfaces(dom) == false then
-		print("Inconsistent grid layouts after first distribution. Aborting.")
-		exit()
-	end
-end
-
---	The different target processes now all have their part of the grid.
---	we'll now perform the 'mid-refinement' before we'll further
---	distribute the domain.
--- perform mid-refinement
-for i = 1, numMidRefs do
-	print("refining...")
-	refiner:refine()
-end
-
---	We'll further distribute it using regular-grid-partitioning again.
---	Process 0 will distribute its grid among processes 0, 1, 2, 3.
---	Process 4 will distribute its grid among processes 4, 5, 6, 7.
---	...
-
-partitionMap:clear()
-if procRank % 2 == 0 and procRank < 4 then
-	-- Specify the first process and the number of processes.
-	-- This will add processes src, src+1, ..., src+n-1
-	partitionMap:add_target_procs(procRank, 2)
-	
-	-- We again use regular-grid partitioning 
-	PartitionDomain_RegularGrid(dom, partitionMap, 1, 2, true)
-	
-	-- again we'll save the partition map. This should only be done for debugging.
-	SaveGrid(dom:grid(), partitionMap:get_partition_handler(),
-		"partitionMap_2_p" .. procRank .. ".ugx")
-end
-
---	The partition map is set up. We can now redistribute the domain
-print("\nPERFORMING REDISTRIBUTION")
-if DistributeDomain(dom, partitionMap, true) == false then
-	print("Second redistribution failed. Please check your partitionMap.")
-	exit()
-end
-
---	We can optionally test the interfaces here.
-if debug == true then
-	if TestDomainInterfaces(dom) == false then
-		print("Inconsistent grid layouts after second distribution. Aborting.")
-		exit()
-	end
-end
-
---	Now perform post-refinement
-for i = 1, numPostRefs do
-	print("refining...")
-	refiner:refine()
-end
-
--- Lets save the domain on each process
+-- save the domain on each process
 outFileName = outFileNamePrefix .. ProcRank() .. ".ugx"
+print("saving domain to " .. outFileName .. "...")
 SaveDomain(dom, outFileName)
 
--- Everything seems to went fine.
-print("Saved domain to " .. outFileName)
 
 
 -- Now lets save the hierarchy on each process
--- The SaveGridHierarchy routine directly works on the domains grid.
+-- The SaveGridHierarchyTransformed routine directly works on the domains grid.
 -- We can access the grid of a domain through its grid() member method.
 --
--- SaveGridHierarchy outputs a grid, where each level is assigned to a subset.
--- Original subsets are not contained in that file.
+-- SaveGridHierarchyTransformed outputs a grid, where each level has an offset
+-- along the z-axis.
+zOffset = 1
 outFileName = outHierarchyFilePrefix .. ProcRank() .. ".ugx"
-if SaveGridHierarchy(dom:grid(), outFileName) == false then
-	print("Saving of grid-hierarch to " .. outFileName .. " failed. Aborting.")
-	exit()
-end
+print("saving grid hierarchy to " .. outFileName)
+SaveGridHierarchyTransformed(dom:grid(), dom:subset_handler(), outFileName, zOffset)
 
--- Again everything went fine.
-print("Saved hierarchy to " .. outFileName)
 
 --------------------------------------------------------------------------------
---	New code starts here
+--	Now setup the laplace problem and solve it
 
 -- Create, Load, Refine and Distribute Domain
 neededSubsets = {"Inner", "Boundary"}
@@ -195,32 +92,41 @@ if util.CheckSubsets(dom, neededSubsets) == false then
 	print("Some subsets are incorrect. Aborting."); exit();
 end
 
-print("subsets checked")
-ug_load_script("tut04_2_disc_laplace.lua")
 
--- Using the AssembleLaplace method which we created in
--- tut04_2_disc_laplace.lua, we now create the linear operator.
--- Note that we use the default callbacks defined in tut04_2_disc_laplace.lua
--- by passing nil. One could instead simply not specify the arguments
--- (this defaults to nil).
--- You could of course create your callbacks analogous to tutorial 3 and
--- pass their names (as strings) to AssembleLaplace.
--- Make sure that you use the ones with the right dimension!
-linOp, approxSpace, domainDisc, dirichletBnd = AssembleLaplace(dom, "Inner", "Boundary", b, nil, nil, nil, nil)
+function DirichletValue(x, y, t)
+	return x*x + y*y
+end
 
-approxSpace:print_layout_statistic()
+approxSpace = ApproximationSpace(dom)
+approxSpace:add_fct("c", "Lagrange", 1)
 
-print("Domain created.")
+-- lets order indices using Cuthill-McKee
+OrderCuthillMcKee(approxSpace, true);
+
+elemDisc = ConvectionDiffusion("c", "Inner", "fv1")
+elemDisc:set_upwind(FullUpwind())
+elemDisc:set_diffusion(1)
+
+dirichletBND = DirichletBoundary()
+dirichletBND:add("DirichletValue", "c", "Boundary")
+
+domainDisc = DomainDiscretization(approxSpace)
+domainDisc:add(elemDisc)
+domainDisc:add(dirichletBND)
 
 
--- Create surface functions (vectors) for Au=b (where A=linOp) and initialize them
+-- Create surface functions (vectors) for Au=b and initialize them
+A = MatrixOperator()
 u = GridFunction(approxSpace)
 b = GridFunction(approxSpace)
 
 
 -- set initial value
 u:set(0)
-linOp:set_dirichlet_values(u)
+domainDisc:adjust_solution(u)
+
+-- assemble matrix and rhs
+domainDisc:assemble_linear(A, b)
 
 -----------------
 -- create GMG 
@@ -258,24 +164,13 @@ solver:set_convergence_check(convCheck)
 -------------------------------------------
 --  Apply Solver
 -------------------------------------------
--- 1. init operator
--- ( This is already done in AssembleLaplace, therefore skipped )
---print("Init operator (i.e. assemble matrix).")
---linOp:init()
-
--- 2. init solver for linear Operator
 print("Init solver for operator.")
-solver:init(linOp)
+solver:init(A)
 
--- 3. apply solver
 print("Apply solver.")
 solver:apply_return_defect(u,b)
 
--- 4. print solution to vtk file format
 WriteGridFunctionToVTK(u, "Solution")
-
---	New code ends here
---------------------------------------------------------------------------------
 
 -- we're done.
 print("")
